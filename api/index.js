201a05a987b1bdd5cc92ac726b4b020be6110af2
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2024-09-27 22:10:08 +0530

    vercel file added

--- api/index.js
+++ api/index.js
@@ -40,7 +40,9 @@ app.use(express.static(path.join(__dirname, '/client/dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
-
+app.get("/", (req, res) => {
+  res.send("welcome in  Donocare Services");
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
