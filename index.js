2822e5f95cfc1ee9aad8a424ba7a80e81d20cea6
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2024-09-27 23:34:42 +0530

    cors added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+const cors = require("cors");
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
@@ -26,6 +27,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
