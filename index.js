772d889cf2c7df8124fc7afc1e6bae1a030caf2c
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2024-09-28 20:29:05 +0530

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-const cors = require("cors");
+import cors from 'cors';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
